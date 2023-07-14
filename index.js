7a4f5c17fd003c66f29a129c102e2e7a7e1106c4
d2rhell <ivan1302007x2@gmail.com>
2023-07-14 19:45:11 +0300

    Theme change

---
+++ index.js
@@ -0,0 +1,17 @@
+let theme="white";
+function changeTheme(){
+    let theme1 = document.querySelector('.nav');
+    let theme2 = document.querySelector('.main');
+    let theme3 = document.querySelector('.info');
+    if(theme == "white"){
+        theme1.classList.remove('nav_dark');
+        theme2.classList.remove('main_dark');
+        theme3.classList.remove('info_dark');
+        theme="dark";
+    }else{
+        theme1.classList.add('nav_dark');
+        theme2.classList.add('main_dark');
+        theme3.classList.add('info_dark');
+        theme="white";
+    }
+}
