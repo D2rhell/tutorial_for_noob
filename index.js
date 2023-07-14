f6e30458305cf1db9e0030730fb5167380f0fea7
d2rhell <ivan1302007x2@gmail.com>
2023-07-14 23:19:33 +0300

    normal fix code

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
+const button = document.querySelector(".main_button");
+
+button.addEventListener("click", () => {
+  changeTheme();
+});
+
 let theme = "white";
+const nav = document.querySelector(".nav");
+const main = document.querySelector(".main");
+const info = document.querySelector(".info");
+
 function changeTheme() {
-  let theme1 = document.querySelector(".nav");
-  let theme2 = document.querySelector(".main");
-  let theme3 = document.querySelector(".info");
   if (theme == "white") {
-    theme1.classList.remove("nav_dark");
-    theme2.classList.remove("main_dark");
-    theme3.classList.remove("info_dark");
+    nav.classList.remove("nav_dark");
+    main.classList.remove("main_dark");
+    info.classList.remove("info_dark");
     theme = "dark";
   } else {
-    theme1.classList.add("nav_dark");
-    theme2.classList.add("main_dark");
-    theme3.classList.add("info_dark");
+    nav.classList.add("nav_dark");
+    main.classList.add("main_dark");
+    info.classList.add("info_dark");
     theme = "white";
   }
 }
-// setInterval(function changeTheme() {
-//   let theme1 = document.querySelector(".nav");
-//   let theme2 = document.querySelector(".main");
-//   let theme3 = document.querySelector(".info");
-//   if (theme == "white") {
-//     theme1.classList.remove("nav_dark");
-//     theme2.classList.remove("main_dark");
-//     theme3.classList.remove("info_dark");
-//     theme = "dark";
-//   } else {
-//     theme1.classList.add("nav_dark");
-//     theme2.classList.add("main_dark");
-//     theme3.classList.add("info_dark");
-//     theme = "white";
-//   }
-// }, 1000); - я в теме аллоооооооооо
