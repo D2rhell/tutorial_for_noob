f2b0c231efa4817cbef736234f3e44b88df1b02c
d2rhell <ivan1302007x2@gmail.com>
2023-07-15 20:56:52 +0300

    Correct some mistakes

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const button = document.querySelector(".main_button");
 
 button.addEventListener("click", () => {
-  changeTheme();
+  changeTheme(theme_);
 });
 
-let theme = "white";
+let theme_ = "white";
 const nav = document.querySelector(".nav");
 const main = document.querySelector(".main");
 const info = document.querySelector(".info");
 
-function changeTheme() {
+function changeTheme(theme) {
   if (theme == "white") {
     nav.classList.remove("nav_dark");
     main.classList.remove("main_dark");
     info.classList.remove("info_dark");
-    theme = "dark";
+    theme_ = "dark";
   } else {
     nav.classList.add("nav_dark");
     main.classList.add("main_dark");
     info.classList.add("info_dark");
-    theme = "white";
+    theme_ = "white";
   }
 }
