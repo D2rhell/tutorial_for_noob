811c81dc3ee7560a45ee20a708dfb1c0f5682da0
runeWarrier <ivan13012007x2@gmail.com>
2023-08-02 15:48:49 +0300

    Add nav bar dark and selected class

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ let theme_ = "white";
 const nav = document.querySelector(".nav");
 const main = document.querySelector(".main");
 const info = document.querySelector(".info");
+const event2 = [...[...document.querySelectorAll(".nav__item")].slice(1)];
+const event3 = document.querySelector(".nav__burger");
 
 function changeTheme(theme) {
   if (theme == "white") {
     nav.classList.remove("nav_dark");
     main.classList.remove("main_dark");
     info.classList.remove("info_dark");
+    event2.map((i) => i.classList.remove("nav__item_dark"));
+    event3.classList.remove("nav__burgerDark");
     theme_ = "dark";
   } else {
     nav.classList.add("nav_dark");
     main.classList.add("main_dark");
     info.classList.add("info_dark");
+    event2.map((i) => i.classList.add("nav__item_dark"));
+    event3.classList.add("nav__burgerDark");
     theme_ = "white";
   }
 }
@@ -23,5 +29,11 @@ button.addEventListener("click", () => {
   changeTheme(theme_);
 });
 
-document.querySelector(".nav__item").style.background = "#fc8b40";
-document.querySelector(".nav__item").style.color = "#fff";
+const event1 = [...document.querySelectorAll(".nav__button")];
+
+event1.map((j) =>
+  j.addEventListener("click", function () {
+    event1.map((i) => i.classList.remove("nav__item_selected"));
+    this.classList.add("nav__item_selected");
+  })
+);
