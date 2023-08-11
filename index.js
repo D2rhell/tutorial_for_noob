95708b34ffb843a920251c7d4f871f8c3ed2f419
runeWarrier <ivan13012007x2@gmail.com>
2023-08-11 18:22:20 +0300

    Delete some files and fix class nav__item_dark

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ function changeTheme(theme) {
     nav.classList.remove("nav_dark");
     main.classList.remove("main_dark");
     info.classList.remove("info_dark");
-    event2.map((i) => i.classList.remove("nav__item_dark"));
+    event2.forEach((i) => i.classList.remove("nav__item_dark"));
     event3.classList.remove("nav__burgerDark");
     theme_ = "dark";
   } else {
     nav.classList.add("nav_dark");
     main.classList.add("main_dark");
     info.classList.add("info_dark");
-    event2.map((i) => i.classList.add("nav__item_dark"));
+    event2.forEach((i) => i.classList.add("nav__item_dark"));
     event3.classList.add("nav__burgerDark");
     theme_ = "white";
   }
@@ -31,11 +31,11 @@ button.addEventListener("click", () => {
 
 const event1 = [...document.querySelectorAll(".nav__item")];
 
-event1.map((j) =>
+event1.forEach((j) =>
   j.addEventListener("click", function () {
-    event1.map((i) => {
+    event1.forEach((i) => {
       i.classList.remove("nav__item_selected");
     });
-    this.classList.add("nav__item_selected");
+    j.classList.add("nav__item_selected");
   })
 );
