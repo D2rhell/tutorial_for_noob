66b38660cafa248aa814d378e978bd7d3ae8bbdb
runeWarrier <ivan13012007x2@gmail.com>
2023-08-12 21:43:56 +0300

    Complete nav bar

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-let theme_ = "white";
+let theme = "white";
 const nav = document.querySelector(".nav");
 const main = document.querySelector(".main");
 const info = document.querySelector(".info");
-const event2 = [...document.querySelectorAll(".nav__item")];
-const event3 = document.querySelector(".nav__burger");
+const navItems = document.querySelectorAll(".nav__item");
+const navBurger = document.querySelector(".nav__burger");
 
-function changeTheme(theme) {
+function changeTheme() {
   if (theme == "white") {
     nav.classList.remove("nav_dark");
     main.classList.remove("main_dark");
     info.classList.remove("info_dark");
-    event2.forEach((i) => i.classList.remove("nav__item_dark"));
-    event3.classList.remove("nav__burgerDark");
-    theme_ = "dark";
+    navItems.forEach((i) => {
+      i.classList.remove("nav__item_dark");
+    });
+    theme = "dark";
   } else {
     nav.classList.add("nav_dark");
     main.classList.add("main_dark");
     info.classList.add("info_dark");
-    event2.forEach((i) => i.classList.add("nav__item_dark"));
-    event3.classList.add("nav__burgerDark");
-    theme_ = "white";
+    navItems.forEach((i) => {
+      i.classList.add("nav__item_dark");
+    });
+    theme = "white";
   }
 }
 
 const button = document.querySelector(".main__button");
 
 button.addEventListener("click", () => {
-  changeTheme(theme_);
+  changeTheme();
 });
 
-const event1 = [...document.querySelectorAll(".nav__item")];
-
-event1.forEach((j) =>
-  j.addEventListener("click", function () {
-    event1.forEach((i) => {
+navItems.forEach((navItem) =>
+  navItem.addEventListener("click", function () {
+    navItems.forEach((i) => {
       i.classList.remove("nav__item_selected");
     });
-    j.classList.add("nav__item_selected");
+    navItem.classList.add("nav__item_selected");
   })
 );
